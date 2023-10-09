e3ce881400757fda3c733bb91e30a69442ae4ffe
Guillermo956 <146474594+Guillermo956@users.noreply.github.com>
2023-10-09 20:25:50 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
-from app1.controllers import principe_de_paz
-from app1.controllers import users
+from app1.controllers import principe_de_paz,users
 from app1 import app
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
